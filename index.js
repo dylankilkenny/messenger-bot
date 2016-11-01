2381f8148f541171f2b83e1828b0a9a0b731bc90
dylankilkenny <dylankilkenny95@gmail.com>
2016-11-01 17:36:37 +0000

    added speaking

--- index.js
+++ index.js
@@ -30,3 +30,18 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAYuZBcWL0kEBAILZAYlhmDfiffuhORXL7cZBmZAVGTHxO1JmNiapKZARUupv0PoZCZAFGILvvS1XVQtxBsSt3RCLSLZAL63TU6nGZBh6C84XI10xn7k8ZCXuJFQYsTWZCLCF4ZCbCvM3ojmuxyXqxfv65aKdS3jWiGpawM3WezquaeTugZDZD"
